06049908927415fe9d30c73828151ceb3fa2bfa0
Zhengtianqi <270490096@qq.com>
2018-12-18 23:12:55 +0800

    commit

---
+++ src/com/bjut/TestStatic/Singleton.java
@@ -0,0 +1,12 @@
+package com.bjut.TestStatic;
+
+public class Singleton {
+    private static Singleton instance = null;
+    private Singleton(){}
+    public static Singleton getInstance(){
+        if(instance == null){
+            instance = new Singleton();
+        }
+        return instance;
+    }
+}
