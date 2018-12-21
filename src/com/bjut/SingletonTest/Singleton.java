6d9790a0ce74fa9cdd2b30f6116831ff03744a48
Zhengtianqi <270490096@qq.com>
2018-12-21 23:14:13 +0800

    commit

---
+++ src/com/bjut/SingletonTest/Singleton.java
@@ -0,0 +1,9 @@
+package com.bjut.SingletonTest;
+
+public class Singleton {
+    private Singleton(){}
+    private static Singleton instance;
+    public Singleton getInstance(){
+        return instance;
+    }
+}
