b3eec8a69d03c014afe3d2270f3d010c4c2dcaf0
Zhengtianqi <270490096@qq.com>
2018-12-28 17:25:45 +0800

    commit

--- src/com/bjut/TheWorldOfConcurrent/Chapter2/SimpleWN.java
+++ src/com/bjut/TheWorldOfConcurrent/Chapter2/SimpleWN.java
@@ -1,5 +1,13 @@
 package com.bjut.TheWorldOfConcurrent.Chapter2;
-
+/**
+ * 两个线程T1 T2
+ * T1执行object.wait()时，它持有object的锁
+ * wait()方法执行后，T1会等待，并释放object锁。
+ *
+ * T2在执行notify()之前也会先获得object的对象锁，
+ * 当T2执行了notify()方法，会通知T1，会首先尝试重新获得object的对象锁
+ *
+* */
 public class SimpleWN {
     final static Object object = new Object();
     public static class T1 extends Thread{
