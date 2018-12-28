a753083738ea444c10aff87a365b41185eeb9bd4
Zhengtianqi <270490096@qq.com>
2018-12-28 11:32:33 +0800

    commit

--- src/com/bjut/TheWorldOfConcurrent/Chapter2/InterruptThread.java
+++ src/com/bjut/TheWorldOfConcurrent/Chapter2/InterruptThread.java
@@ -2,6 +2,8 @@ package com.bjut.TheWorldOfConcurrent.Chapter2;
 /**
 * 只显示的让t1线程interrupt  只能让线程进行中断
 * 但是必须为它增加相应的中断处理代码，即判断线程是否处于中断的状态，然后break的方式来结束线程
+ *
+ * Thread.isInterrupted()函数判断当前是否被中断了，如果是，则退出循环体，结束线程。
  * */
 public class InterruptThread {
     public static void main(String[] args) throws InterruptedException {
