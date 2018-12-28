66dac3a014e46994952f92107892e8e3e0b9eb41
Zhengtianqi <270490096@qq.com>
2018-12-28 21:11:12 +0800

    commit

--- src/com/bjut/TheWorldOfConcurrent/Chapter2/NoVisibility.java
+++ src/com/bjut/TheWorldOfConcurrent/Chapter2/NoVisibility.java
@@ -1,5 +1,10 @@
 package com.bjut.TheWorldOfConcurrent.Chapter2;
-
+/**
+*   因为此时在Server模式下：
+ *      ReaderThread线程无法“看到”主线程的修改，导致ReaderThread永远无法退出
+ *  解决方法：
+ *      使用volatile来申明ready变量，告诉虚拟机，这个变量可能会在不同的线程中修改。
+* */
 public class NoVisibility {
     private static boolean ready;
     private static int number;
